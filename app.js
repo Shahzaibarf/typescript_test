13d7713b8320efacf4038a03a4dafcff4f70ed35
shahzaib <shahzaib.arif@programmershut.com>
2024-01-21 11:50:56 +0500

    testing

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-console.log("run");
+console.log("run1");
 console.log("run2");
 console.log("run3");
 console.log("run4");
 console.log("run5");
-<<<<<<< HEAD
-=======
 console.log("run6");
 console.log("run7");
->>>>>>> 4a53a973ed77132a4c61bfaff60b3b09c850b84a
+console.log("run8");
+console.log("run9");
+console.log("run10");
